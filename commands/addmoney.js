b4f74a4e36609a641885df05f6a5524fc7147e13
Suepaphly <tabenterprise@gmail.com>
2022-10-19 19:59:15 -0500

    Create addmoney.js

---
+++ commands/addmoney.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+
+exports.run = async (client, message, args) => {
+   const ownerID = [
+    "367445249376649217"
+  ];
+  if (!ownerID.includes(message.author.id)) return;
+
+  let user = message.mentions.members.first() || message.author;
+
+    if (isNaN(args[1])) return;
+    db.add(`money_${user.id}`, args[1])
+    let bal = await db.fetch(`money_${user.id}`)
+    message.channel.send(`Added \`${args[1]}\` kopeks to **${user}**'s balance.\n> Current balance: \`${bal}\` credits.`)
+
+};
+
+module.exports.help = {
+  name:"addmoney",
+  aliases: ["addcredits", "addkopeks"]
+}
