1c07a880976f6f3124db38540d0f62a75f0a55c0
Suepaphly <tabenterprise@gmail.com>
2022-10-19 20:05:41 -0500

    Create daily.js

---
+++ commands/daily.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+const ms = require("parse-ms");
+
+module.exports.run = async (client, message, args) => {
+
+  let user = message.author;
+
+  let timeout = 46000000;
+  let amount = 100;
+
+   let daily = await db.fetch(`daily_${user.id}`);
+
+  if (daily !== null && timeout - (Date.now() - daily) > 0) {
+    let time = ms(timeout - (Date.now() - daily));
+  
+    message.channel.send(`**${user.username}**, daily kopeks reset in \`${time.hours} hours, ${time.minutes} minutes, ${time.seconds} seconds\`.`)
+  } else {
+
+  message.channel.send(`**${user.username}**, you received your \`${amount}\` daily kopeks!`)
+
+  db.add(`money_${user.id}`, amount)
+  db.set(`daily_${user.id}`, Date.now())
+
+  }
+};
+
+module.exports.help = {
+  name:"daily",
+  aliases: ["daylies"]
+}
