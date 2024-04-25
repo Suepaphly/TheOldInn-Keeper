523484cecf4210a82dd9522810651bf77568f7bb
Suepaphly <tabenterprise@gmail.com>
2024-04-25 05:43:31 -0500

    check

--- commands/checklvl.js
+++ commands/checklvl.js
@@ -1,59 +1,34 @@
-const Discord = require('discord.js')
-const db = require('quick.db')
+const { QuickDB } = require("quick.db");
+const db = new QuickDB();
 
 module.exports.run = async (bot, message, args) => {
-   
-    const resp = db.all();
-    var lvls = "";
+    let lvls = "";
 
-    // Sort from higher to lower
+    // Retrieve all data from the database
+    const resp = Object.entries(db.all())
+        .filter(([key, value]) => key.includes(`level_${message.author.id}`))
+        .map(([key, value]) => ({ ID: key, data: value }));
+
+    // Sort data from higher to lower
     resp.sort((a, b) => (a.data < b.data) ? 1 : -1);
-    lvls = resp.filter((item) => item.ID.includes(`level_${message.author.id}`));
 
     let content = "";
-   
-   if (lvls.length === 0){
-      content += `**No Levels**`;     
-   }
-   
-   for (let i = 0; i < lvls.length; i++) {
-        let user = lvls[i].ID.split('level_')[0];
-        if (user === "thief") {
-          let lev = await db.fetch(`thieflevel_${message.author.id}`);
-          content += `${i+1}. **Robbery Level** => ${lev}\n`;
-          
-        } else if (user === "gathering" ) {
-          let lev = await db.fetch(`gatheringlevel_${message.author.id}`);
-          content += `${i+1}. **Gathering Level** => ${lev}\n`;
-          
-        } else if (user === "fishing" ) {
-          let lev = await db.fetch(`fishinglevel_${message.author.id}`);
-          content += `${i+1}. **Fishing Level** => ${lev}\n`;
-          
-        } else if (user === "hunting" ) {
-          let lev = await db.fetch(`huntinglevel_${message.author.id}`);
-          content += `${i+1}. **Hunting Level** => ${lev}\n`;
-          
-        } else if (user === "crafting" ) {
-          let lev = await db.fetch(`craftinglevel_${message.author.id}`);
-          content += `${i+1}. **Crafting Level** => ${lev}\n`;
-          
-        } else if (user === "working" ) {
-          let lev = await db.fetch(`workinglevel_${message.author.id}`);
-          content += `${i+1}. **Working Level** => ${lev}\n`;
-          
-        }
+
+    if (resp.length === 0) {
+        content += `**No Levels**`;
     }
 
-    const embed = new Discord.MessageEmbed()
-    .setDescription(`**${message.author.username} Skill Levels **\n\n${content}`)
-    .setColor("#FFFFFF")
+    for (let i = 0; i < resp.length; i++) {
+        let user = resp[i].ID.split('level_')[0];
+        let lev = await db.get(`${user}level_${message.author.id}`);
+        let levelType = user.charAt(0).toUpperCase() + user.slice(1);
+        content += `${i + 1}. <@${message.author.id}> **${levelType} Level** => ${lev}\n`;
+    }
 
-    message.channel.send(embed)
-   }
-
+    message.channel.send(content);
+};
 
 module.exports.help = {
-    name:"checklvls",
-    aliases: ["lvls", "stat", "stats"]
-}
+    name: "checklvls",
+    aliases: ["check", "lvls", "stat", "stats"]
+};
