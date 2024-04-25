31c8ca2f57b4929833622a16bfc674e5cdbba351
Suepaphly <tabenterprise@gmail.com>
2024-04-25 05:24:57 -0500

    update BJ and balance and Bank

--- commands/balance.js
+++ commands/balance.js
@@ -6,6 +6,7 @@ module.exports.run = async (client, message, args) => {
     let user = message.mentions.members.first() || message.author;
     const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
 
+    // Use db.get and await for fetching data asynchronously
     let bal = await db.get(`money_${user.id}`);
     let bbal = await db.get(`bank_${user.id}`);
 
@@ -13,7 +14,7 @@ module.exports.run = async (client, message, args) => {
     if (bal === null) bal = 0;
     if (bbal === null) bbal = 0;
 
-    message.channel.send(`**${member.user.tag}** Wallet: \`${bal}\` kopeks; Bank: \`${bbal}\``);
+    message.channel.send(`**<@${message.author.id}>'s'** Wallet: \`${bal}\` kopeks; Bank: \`${bbal}\``);
 }; 
 
 module.exports.help = {
--- commands/bank.js
+++ commands/bank.js
@@ -14,27 +14,28 @@ exports.run = async (client, message, args) => {
     let user = message.author;
     const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
 
-    
+    // Replace fetch with get and handle asynchronously
     let wbal = await db.get(`money_${user.id}`);
     let bbal = await db.get(`bank_${user.id}`);
+    if (bbal === null) bbal = 0;
     let deposit = Math.floor(Math.abs(parseInt(args[0])));
 
     let timer = await db.get(`deposit_${user.id}`);
     let timeout = 21600000;
 
     if (!deposit || deposit === 0) {
-        message.channel.send(`**${user.username} Bank Balance: ${bbal}**`);
+        message.channel.send(`**<@${user.id}>'s Bank Balance: ${bbal}**`);
     } else if (timeout - (Date.now() - timer) > 9000) {
         let time = ms(timeout - (Date.now() - timer));
         message.channel.send(`Bank Balance: ${bbal} **${member.user.tag}**, try again in \`${time.hours} hours, ${time.minutes} minutes, ${time.seconds} seconds\`.`);
     } else {
         if (deposit <= wbal) {
-            
+            // Replace subtract with sub and handle asynchronously
             await db.sub(`money_${user.id}`, deposit);
             await db.add(`bank_${user.id}`, deposit);
             let newbal = await db.get(`bank_${user.id}`);
             await db.set(`deposit_${user.id}`, Date.now());
-            message.channel.send(`**${member.user.tag}** just deposited '${deposit}' into their bank. Their new bank balance is '${newbal}'`);
+            message.channel.send(`**<@${user.id}>** just deposited '${deposit}' into their bank. Their new bank balance is '${newbal}'`);
         } else {
             message.channel.send(`${user.username}, please send a valid amount of Kopeks.`);
         }
