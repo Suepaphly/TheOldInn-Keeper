2c409218eb611bbb35341761dff94305df67e932
Suepaphly <tabenterprise@gmail.com>
2024-04-25 05:33:18 -0500

    update buy

--- commands/buy.js
+++ commands/buy.js
@@ -1,5 +1,5 @@
-const db = require("quick.db");
-const { EmbedBuilder } = require("discord.js");
+const { QuickDB } = require("quick.db");
+const db = new QuickDB();
 const ptt = require("../utility/protectTheTavern.js");
 
 module.exports.run = async (client, message, args) => {
@@ -7,17 +7,14 @@ module.exports.run = async (client, message, args) => {
     const item = args[0];
     const amount = args[1];
     const location = args[2];
-    const money = await db.fetch(`money_${message.author.id}`);
+    const money = await db.get(`money_${message.author.id}`);
 
-    if (item == null || amount == null) {
-        const buyEmbed = new EmbedBuilder()
-            .setColor('#000001')
-            .setTitle("Buy Castle Walls, Army Troops, and Defensive Traps.")
-            .setDescription(`Just type =buy [wall, army, or trap] [amount] [Traps and Army: Choose Rampart, Wall, or Castle].
-                             \n You can leave the amount blank to see prices and current amounts.
-                             \n Ex: =buy town_gaurd 5 rampart; =buy rampart 2; =buy boiling_oil 4 castle`)
-            .setFooter({ text: 'The Tavernkeeper thanks you for playing. \n' });
-        message.channel.send({ embeds: [buyEmbed] });
+    if (!item || !amount) {
+        message.channel.send(`<@${user.id}>, Buy Castle Walls, Army Troops, and Defensive Traps.
+                             Just type =buy [wall, army, or trap] [amount] [Traps and Army: Choose Rampart, Wall, or Castle].
+                             You can leave the amount blank to see prices and current amounts.
+                             Ex: =buy town_guard 5 rampart; =buy rampart 2; =buy boiling_oil 4 castle
+                             The Tavernkeeper thanks you for playing.`);
     } else {
         if (ptt.troopArray.includes(item) && ptt.wallArray.includes(location)) {
             ptt.buyArmy(item, amount, location, user, message);
@@ -26,7 +23,7 @@ module.exports.run = async (client, message, args) => {
         } else if (ptt.wallArray.includes(item)) {
             ptt.buyWall(item, amount, user, message);
         } else {
-            message.channel.send("Make sure you set the location! Ex: =buy boiling_oil 1 castle");
+            message.channel.send(`<@${user.id}>, Make sure you set the location! Ex: =buy boiling_oil 1 castle`);
         }
     }
 }
