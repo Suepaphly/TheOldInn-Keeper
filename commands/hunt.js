41bf29c1ee2d25b3b341eff84424235e45626f4f
Suepaphly <tabenterprise@gmail.com>
2024-04-25 06:18:32 -0500

    hunt

--- commands/hunt.js
+++ commands/hunt.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
-const db = require("quick.db");
+const { QuickDB } = require("quick.db");
+const db = new QuickDB();
 const mg = require("../utility/utility.js");
 
 module.exports.run = async (client, message, args) => {
-
   let ms;
   try {
     ms = (await import("parse-ms")).default;
@@ -12,72 +12,79 @@ module.exports.run = async (client, message, args) => {
     return;
   }
 
-    const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
-    let user = message.author;
-    let author = await db.fetch(`hunt_${user.id}`)
- let userlevel = await db.fetch(`huntinglevel_${user.id}`)
+  const member =
+    message.mentions.members.first() ||
+    message.guild.members.cache.get(args[0]) ||
+    message.member;
+  let user = message.author;
+  let author = await db.get(`hunt_${user.id}`);
+  let userlevel = await db.get(`huntinglevel_${user.id}`);
 
-    let timeout = 3600000;
-    
-    if (author !== null && timeout - (Date.now() - author) > 0) {
-        let time = ms(timeout - (Date.now() - author));
-    
-    
-        message.channel.send(`**${member.user.tag}**, you already hunted recently, try again in \`${time.minutes} minutes, ${time.seconds} seconds\`.`)
-      } else {
+  let timeout = 3600000;
 
+  if (author !== null && timeout - (Date.now() - author) > 0) {
+    let time = ms(timeout - (Date.now() - author));
 
+    message.channel.send(
+      `**<@${message.author.id}>**, you already hunted recently, try again in \`${time.minutes} minutes, ${time.seconds} seconds\`.`
+    );
+  } else {
     let rarehunt = [
-        "**🦃 `(Turkey)`**", 
-        "**🐃 `(Water Buffalo)`**",
-        "**:bison: `(Bison)`**",
-        "**:zebra: `(Zebra)`**",
-        "**:kangaroo: `(Kangaroo)`**"
-    ]
-    
+      "**🦃 `(Turkey)`**",
+      "**🐃 `(Water Buffalo)`**",
+      "**:bison: `(Bison)`**",
+      "**:zebra: `(Zebra)`**",
+      "**:kangaroo: `(Kangaroo)`**",
+    ];
+
     let bighunt = [
-        "**:sheep: `(Sheep)`**",
-        "**🐂 `(Ox)`**",
-        "**🐄 `(Cow)`**",
-        "**🐖 `(Pig)`**",
-        "**:deer: `(Deer)`**"
-    ]
-    
+      "**:sheep: `(Sheep)`**",
+      "**🐂 `(Ox)`**",
+      "**🐄 `(Cow)`**",
+      "**🐖 `(Pig)`**",
+      "**:deer: `(Deer)`**",
+    ];
+
     let hunt = [
-        "**🐰 `(Rabbit)`**",
-        "**🐔 `(Chicken)`**",
-        "**🐍 `(Snake)`**",
-        "**:goat: `(Goat)`**",
-        "**:duck:  `(Duck)`**"
-    ]
-    
+      "**🐰 `(Rabbit)`**",
+      "**🐔 `(Chicken)`**",
+      "**🐍 `(Snake)`**",
+      "**:goat: `(Goat)`**",
+      "**:duck:  `(Duck)`**",
+    ];
+
     let trash = [
-        "**🐰 `(Rabbit)`**",
-        "**🐸 `(Frog)`**",
-        "**🐓 `(Rooster)`**",
-        "**🐿 `(Chipmunk)`**",
-        "**:rat: `(Rat)`**"
-    ]
- 
+      "**🐰 `(Rabbit)`**",
+      "**🐸 `(Frog)`**",
+      "**🐓 `(Rooster)`**",
+      "**🐿 `(Chipmunk)`**",
+      "**:rat: `(Rat)`**",
+    ];
 
-   var fisharray = [trash, hunt, bighunt, rarehunt];
-   var fishresult;
-       
-   if(userlevel != null){ 
+    var fisharray = [trash, hunt, bighunt, rarehunt];
+    var fishresult;
+
+    if (userlevel != null) {
       fishresult = mg.skillMinigame("hunt", userlevel);
-   } else {
+    } else {
       fishresult = mg.skillMinigame("hunt", 0);
-   }
-        message.channel.send(`**HUNT MINIGAME:** - 🏹\n**${member.user.tag}** has hunted a ${fisharray[fishresult[0]][fishresult[1]]} and earned \`${fishresult[2]}\` kopeks.`)
-
-    db.add(`money_${user.id}`, fishresult[2])
-    db.set(`hunt_${user.id}`, Date.now())
-
-    };
     }
 
+    message.channel.send(
+      `**HUNT MINIGAME:** - 🏹\n**<@${message.author.id}>** has hunted a ${
+        fisharray[fishresult[0]][fishresult[1]]
+      } and earned \`${fishresult[2]}\` kopeks.`
+    );
+
+    // Update the user's money
+    let currentMoney = await db.get(`money_${user.id}`);
+    await db.set(`money_${user.id}`, currentMoney + fishresult[2]);
+    // Set the time of the last hunt
+    await db.set(`hunt_${user.id}`, Date.now());
+  }
+};
 
 module.exports.help = {
-  name:"hunt",
-  aliases: []
-}
+  name: "hunt",
+  aliases: [],
+};
