0332bc7d4fd0627cdb9060807307cc5ac5358581
Suepaphly <tabenterprise@gmail.com>
2024-04-26 04:37:20 -0500

    leaderboard

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,39 +1,35 @@
-const Discord = require('discord.js')
-const db = require('quick.db')
+const Discord = require('discord.js');
+const { QuickDB } = require("quick.db");
+const db = new QuickDB();
 
 module.exports.run = async (bot, message, args) => {
-   
-    const resp = db.all();
-    var money = "";
+    // Fetch all entries
+    const allEntries = await db.all();
 
-    // Sort from higher to lower
-    resp.sort((a, b) => (a.data < b.data) ? 1 : -1);
-    money = resp.filter((item) => item.ID.includes("bank_"));
+    // Filter entries that start with "bank_" and map them into [key, value] format
+    const bankEntries = allEntries
+        .filter(entry => entry.id.startsWith("bank_"))
+        .map(entry => [entry.id, entry.value]);
+
+    // Sort the bank entries by value in descending order
+    bankEntries.sort((a, b) => b[1] - a[1]);
 
     let content = "";
-    var list = 11;
-    if(money.length < 10){
-      list = money.length;  
+    const maxEntries = Math.min(bankEntries.length, 5);
+
+    for (let i = 0; i < maxEntries; i++) {
+        const [key, amount] = bankEntries[i];
+        const userId = key.split('_')[1];
+        const user = bot.users.cache.get(userId);
+        if (user) {
+            const formattedAmount = amount.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+            content += `${i + 1}. **${user.tag}** => ${formattedAmount}\n`;
+        }
     }
-   
-    for (let i = 0; i < 5; i++) {
-        let user = bot.users.cache.get(money[i].ID.split('_')[1]).tag
-
-        var conv = money[i].data.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
-
-        content += `${i+1}. **${user}** => ${conv}\n`
-    
-    }
-
-    const embed = new Discord.MessageEmbed()
-    .setDescription(`**${message.guild.name} Kopek Leaderboard (In Bank) **\n\n${content}`)
-    .setColor("#FFFFFF")
-
-    message.channel.send(embed)
-   }
-
+  message.channel.send(`**${message.guild.name} Kopek Leaderboard (In Bank)**\n\n${content}`);
+};
 
 module.exports.help = {
-    name:"leaderboard",
+    name: "leaderboard",
     aliases: ["top"]
-}
+};
