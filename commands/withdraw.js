e3fd66372be607ce99ce7fbfb8a5eb5bfeeb40f0
Suepaphly <tabenterprise@gmail.com>
2024-04-28 05:55:13 -0500

    Update withdraw.js

--- commands/withdraw.js
+++ commands/withdraw.js
@@ -1,4 +1,5 @@
-const db = require("quick.db") 
+const { QuickDB } = require("quick.db");
+const db = new QuickDB();
 const Discord = require("discord.js");
 const mg = require("../utility/utility.js");
 
@@ -15,22 +16,22 @@ module.exports.run = async (client, message, args) => {
     let user = message.author;
     const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
 
-    let bbal = await db.fetch(`bank_${user.id}`);
+    let bbal = await db.get(`bank_${user.id}`);
     let withdraw = Math.floor(Math.abs(parseInt(args[0])));
 
-    let timer = await db.fetch(`deposit_${user.id}`)
+    let timer = await db.get(`deposit_${user.id}`)
 
     if(!withdraw || withdraw === null || withdraw === 0 || args[0] === "all"){ 
             let total = await db.fetch(`bank_${user.id}`);
-            await db.subtract(`bank_${user.id}`, total);
+            await db.sub(`bank_${user.id}`, total);
             await db.add(`money_${user.id}`, total);
-            let newbal = await db.fetch(`money_${user.id}`);
+            let newbal = await db.get(`money_${user.id}`);
             message.channel.send(`**${member.user.tag}** just withdrew \'${total}\' into their wallet. Their new wallet balance is \'${newbal}\'`);
     } else {      
         if(withdraw <= bbal) {
-            await db.subtract(`bank_${user.id}`, withdraw);
+            await db.sub(`bank_${user.id}`, withdraw);
             await db.add(`money_${user.id}`, withdraw);
-            let newbbal = await db.fetch(`bank_${user.id}`);
+            let newbbal = await db.get(`bank_${user.id}`);
             message.channel.send(`**${member.user.tag}** just withdrew \'${withdraw}\' into their wallet. Their new bank balance is \'${newbbal}\'`);
         } else {
             message.channel.send(`${user.username} Please send a valid amount of Kopeks`);
